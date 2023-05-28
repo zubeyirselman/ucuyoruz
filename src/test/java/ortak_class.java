bf9e388537966a072519fef508076ab5e2f3154a
zsk <zsk199207@gmail.com>
2023-05-28 22:28:20 +0300

    first commit

---
+++ src/test/java/ortak_class.java
@@ -0,0 +1,5 @@
+public class ortak_class {
+    public static void main(String[] args) {
+        System.out.println("sıkıntı yok");
+    }
+}
---
+++ src/test/java/zubeyir.java
@@ -0,0 +1,13 @@
+public class zubeyir {
+    public static void main(String[] args) {
+        System.out.println("kendi mekanim");
+    }
+}
+/*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zubeyirselman/ucuyoruz.git
+git push -u origin main
+ */
