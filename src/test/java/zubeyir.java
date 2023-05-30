0798d48766d239f92b29c7ee81dc38ae1d3c40dd
zsk <zsk199207@gmail.com>
2023-05-30 20:15:50 +0300

    first commit

--- src/test/java/zubeyir.java
+++ src/test/java/zubeyir.java
@@ -10,6 +10,6 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/zubeyirselman/ucuyoruz.git
+git remote add origin https://github.com/zubeyirselman/com.Team113_Cucumber.git
 git push -u origin main
  */
